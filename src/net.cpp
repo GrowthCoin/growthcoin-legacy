3aa8a1c46fb75c3afab253a7f4b5d52f667a11d6
iamunick <why@iamunick.com>
2014-05-26 22:33:21 -0400

    Activated DNS Seeds

--- src/net.cpp
+++ src/net.cpp
@@ -1152,7 +1152,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"", ""},
+    {"blockx", "seed.grw.blockx.info"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
@@ -1850,18 +1850,11 @@ void StartNode(void* parg)
     // Start threads
     //
 
-/*
     if (!GetBoolArg("-dnsseed", true))
         printf("DNS seeding disabled\n");
     else
         if (!NewThread(ThreadDNSAddressSeed, NULL))
             printf("Error: NewThread(ThreadDNSAddressSeed) failed\n");
-*/
-
-    if (!GetBoolArg("-dnsseed", false))
-        printf("DNS seeding disabled\n");
-    if (GetBoolArg("-dnsseed", false))
-        printf("DNS seeding NYI\n");
 
     // Map ports with UPnP
     if (fUseUPnP)
