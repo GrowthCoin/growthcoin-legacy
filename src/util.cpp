050df8750a8fc368d43c0a26d8fea25b2726fc1c
iamunick <why@iamunick.com>
2014-05-26 21:40:23 -0400

    Update Message Box Buttons

--- src/util.cpp
+++ src/util.cpp
@@ -1292,7 +1292,7 @@ void AddTimeData(const CNetAddr& ip, int64 nTime)
                     string strMessage = _("Warning: Please check that your computer's date and time are correct! If your clock is wrong GrowthCoin will not work properly.");
                     strMiscWarning = strMessage;
                     printf("*** %s\n", strMessage.c_str());
-                    uiInterface.ThreadSafeMessageBox(strMessage+" ", string("GrowthCoin"), CClientUIInterface::OK | CClientUIInterface::ICON_EXCLAMATION);
+                    uiInterface.ThreadSafeMessageBox(strMessage+" ", string("GrowthCoin"), CClientUIInterface::MSG_WARNING);
                 }
             }
         }
