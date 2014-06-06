377b1d8d09e6025f39b2c5da7f1ef2ba70a40dbb
iamunick <why@iamunick.com>
2014-06-05 22:10:16 -0400

    Add Export/Import Wallet to GUI

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -87,6 +87,8 @@ private:
     QAction *exportAction;
     QAction *encryptWalletAction;
     QAction *backupWalletAction;
+    QAction *dumpWalletAction;
+    QAction *importWalletAction;
     QAction *changePassphraseAction;
     QAction *aboutQtAction;
     QAction *openRPCConsoleAction;
@@ -171,6 +173,9 @@ private slots:
     void incomingTransaction(const QModelIndex & parent, int start, int end);
     /** Encrypt the wallet */
     void encryptWallet(bool status);
+    /** Import/Export the wallet's keys */
+    void dumpWallet();
+    void importWallet();
     /** Backup the wallet */
     void backupWallet();
     /** Change encrypted wallet passphrase */
