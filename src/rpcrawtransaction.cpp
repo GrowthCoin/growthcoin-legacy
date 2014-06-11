b4bd680e24ff447156d505c75ecc29b45a999cd9
iamunick <why@iamunick.com>
2014-06-11 19:39:39 -0400

    Add Address to listunspent RPC

--- src/rpcrawtransaction.cpp
+++ src/rpcrawtransaction.cpp
@@ -192,6 +192,13 @@ Value listunspent(const Array& params, bool fHelp)
         int64 nValue = out.tx->vout[out.i].nValue;
         const CScript& pk = out.tx->vout[out.i].scriptPubKey;
         Object entry;
+        CTxDestination address;
+        if (ExtractDestination(out.tx->vout[out.i].scriptPubKey, address))
+        {
+            entry.push_back(Pair("address", CBitcoinAddress(address).ToString()));
+            if (pwalletMain->mapAddressBook.count(address))
+                entry.push_back(Pair("account", pwalletMain->mapAddressBook[address]));
+        }
         entry.push_back(Pair("txid", out.tx->GetHash().GetHex()));
         entry.push_back(Pair("vout", out.i));
         entry.push_back(Pair("scriptPubKey", HexStr(pk.begin(), pk.end())));
