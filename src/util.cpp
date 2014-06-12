9b9c2c49f8287663a33140e797a68ef222e874c1
iamunick <why@iamunick.com>
2014-06-12 13:34:04 -0400

    Add leftrim and remove pid for WIN32

--- src/util.cpp
+++ src/util.cpp
@@ -1226,6 +1226,7 @@ boost::filesystem::path GetPidFile()
     return pathPidFile;
 }
 
+#ifndef WIN32
 void CreatePidFile(const boost::filesystem::path &path, pid_t pid)
 {
     FILE* file = fopen(path.string().c_str(), "w");
@@ -1235,6 +1236,7 @@ void CreatePidFile(const boost::filesystem::path &path, pid_t pid)
         fclose(file);
     }
 }
+#endif
 
 bool RenameOver(boost::filesystem::path src, boost::filesystem::path dest)
 {
