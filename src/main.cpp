8bbcfc3be9c2abe75102e9956e637ea7a21cc33b
iamunick <why@iamunick.com>
2014-07-13 14:06:56 -0400

    Fix fork

--- src/main.cpp
+++ src/main.cpp
@@ -2912,6 +2912,13 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
             return true;
         }
 
+        if (pfrom->nVersion < 60007)
+        {
+            printf("partner %s using an old client %d, disconnecting\n", pfrom->addr.ToString().c_str(), pfrom->nVersion);
+            pfrom->fDisconnect = true;
+            return true;
+        }
+
         // ppcoin: record my external IP reported by peer
         if (addrFrom.IsRoutable() && addrMe.IsRoutable())
             addrSeenByPeer = addrMe;
